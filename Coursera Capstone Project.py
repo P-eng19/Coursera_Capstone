8795f3bdb762f27282051b684e42b77758eabedf
P-eng19 <62846601+P-eng19@users.noreply.github.com>
2020-03-31 00:08:44 +0530

    Add files via upload

---
+++ Coursera Capstone Project.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook will be used for Capstone Project
+
+# ## This project is a part of the Coursera Capstone course
+
+# ### This Project is the first assignment of the Capstone Project Module
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
